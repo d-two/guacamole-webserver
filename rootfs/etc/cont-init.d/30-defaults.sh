63fea3023e925f797dc316b2a0d073ba35a88f42
d-two <32079446+d-two@users.noreply.github.com>
2020-11-23 21:47:23 +0100

    Update 30-defaults.sh

--- rootfs/etc/cont-init.d/30-defaults.sh
+++ rootfs/etc/cont-init.d/30-defaults.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/with-contenv sh
 
-cp -rn /app/guacamole /data
+cp -rnv /app/guacamole /data
+
 mkdir -p /config/guacamole
-cp -rn /config/guacamole /data
+cp -nv /app/guacamole/guacamole.properties /config/guacamole/guacamole.properties
+cp -rv /config/guacamole /data
+
 mkdir -p /root/.data/freerdp/known_hosts
